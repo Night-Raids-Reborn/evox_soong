LineageOS/android_build_soong

cleanbuild: add *.zip.md5sum to installclean_files
Change-Id: I9af8579f4acf5043375650ec4f7fadb66c0eb15e

ui/build/cleanbuild.go
@@ -96,6 +96,7 @@ func installClean(ctx Context, config Config, what int) {
 		hostOut("vts"),
 		productOut("*.img"),
 		productOut("*.zip"),
+		productOut("*.zip.md5"),
 		productOut("android-info.txt"),
 		productOut("kernel"),
 		productOut("data"),

Night-Raids-Reborn/evox_soong

Signed-off-by: Joey Huab <joey@evolution-x.org>

ui/build/cleanbuild.go
@@ -154,6 +154,7 @@ func installClean(ctx Context, config Config) {
 		hostCommonOut("obj/PACKAGING"),
 		productOut("*.img"),
 		productOut("*.zip"),
+		productOut("*.zip.md5sum"),
 		productOut("android-info.txt"),
 		productOut("misc_info.txt"),
 		productOut("apex"),